4e1e51b850cb1e967d1aa263d2a166ef2643c7eb
Zaid Zuhair <zaidzuhairalani@gmail.com>
2024-05-06 17:38:53 +0300

    add login, mainpage templates

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('', views.mainpage, name="mainpage"),
     path('signup/', views.signup, name="signup"),
     path('login/',auth_views.LoginView.as_view(template_name='main/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(next_page='mainpage'), name="logout"),
--- quiz_app/views.py
+++ quiz_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
 
 from .forms import CustomUserCreationForm
+
+@login_required
+def mainpage(request):
+    return render(request,'main/mainpage.html')
 
 def signup(request):
     if request.method == 'POST':
